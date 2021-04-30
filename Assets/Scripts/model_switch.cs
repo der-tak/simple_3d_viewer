9d987983023c1b015ba1109cdf60067b5da73dee
der-tak <Philippkd8@gmail.com>
2021-04-30 23:18:55 +0200

    build problem fixed, added model switcher

---
+++ Assets/Scripts/model_switch.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class model_switch : MonoBehaviour
+{
+    //initialization
+    public GameObject[] modelList;
+    int index;
+
+    void Start()
+    {
+        index = 0;
+        modelList[index].SetActive(true);
+    }
+
+    public void NextModel()
+    {
+        if (index == modelList.Length-1)
+        {
+            index = 0;
+            modelList[modelList.Length-1].SetActive(false);
+            modelList[index].SetActive(true);
+        }
+        else 
+        {
+            modelList[index].SetActive(false);
+            modelList[index+1].SetActive(true);
+            index += 1;
+        }
+        Debug.Log(index);
+    }
+
+    public void PrevModel()
+    {
+        if(index == 0)
+        {
+            index = modelList.Length-1;
+            modelList[0].SetActive(false);
+            modelList[index].SetActive(true);
+        }
+        else
+        {
+            modelList[index].SetActive(false);
+            modelList[index-1].SetActive(true);
+            index -= 1;   
+        }
+        Debug.Log(index);
+    }
+}
